d3372dfebe69b6cbd42db3f1cb319899326a422a
marcelodanelon <marcelodanelon11@gmail.com>
2023-05-17 18:31:22 -0300

    paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, redirect
 from contact.models import Contact
 from django.http import Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -32,9 +37,13 @@ def search(request):
             Q(email__icontains=search_value)
         )\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
     }
 
